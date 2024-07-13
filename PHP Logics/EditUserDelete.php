7e3d6b4ac4b48b6f57b5ddc9e71c105e591e1442
niaz <niaz@gmail.com>
2024-07-13 20:07:10 +0600

    Deleting the thumbnail and avatar when the user is deleted

--- PHP Logics/EditUserDelete.php
+++ PHP Logics/EditUserDelete.php
@@ -1,15 +1,51 @@
 <?php
-   include "../admin/config/database.php";
+//including the database file and getting the id from header
+include "../admin/config/database.php";
+$id = $_GET["id"];
 
-   if(isset($_GET["id"])){
-        $id = $_GET["id"];
-        $sql = "DELETE FROM users WHERE id = '$id'";
-        $result = mysqli_query($conn,$sql);
-        if($result){
-            header("Location: http://localhost/blog/admin/manageUser.php");
-        }else{
-            echo "<script>alert('Failed to remove the user')</script>";
+
+if (isset($id)) {
+    //deleting the user thumbnail
+    $thumbnailSql = "SELECT * FROM posts WHERE userId = '$id'";
+    $thumbnailResult = mysqli_query($conn, $thumbnailSql);
+    if (mysqli_num_rows($thumbnailResult) > 0) {
+        while ($thumbnail = mysqli_fetch_assoc($thumbnailResult)) {
+            $thumbnailPath = "../thumbnail/" . $thumbnail["thumbnail"];
+            if ($thumbnailPath) {
+                unlink($thumbnailPath);
+            }
         }
-   }
+    }
+
+
+
+
+
+    //deleting the avatar
+    $avatarSql = "SELECT * FROM users WHERE id = '$id'";
+    $avatarResult = mysqli_query($conn, $avatarSql);
+    if (mysqli_num_rows($avatarResult) > 0) {
+        while ($avatar = mysqli_fetch_assoc($avatarResult)) {
+            $avatarPath = "../images/" . $avatar["avatar"];
+            if ($avatarPath) {
+                unlink($avatarPath);
+            }
+        }
+    }
+
+
+
+
+
+
+    //deleting the user
+    $sql = "DELETE FROM users WHERE id = '$id'";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        header("Location: http://localhost/blog/admin/manageUser.php");
+    } else {
+        echo "<script>alert('Failed to remove the user')</script>";
+    }
+}
 
 ?>
